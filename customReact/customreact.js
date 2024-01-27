af119d3b61ef8147e35b09d0dbc531675ff2077a
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-27 23:39:23 +0530

    koshish ki

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -3,7 +3,7 @@ function customRender(reactElement, container) {
    domElement.innerHTML = reactElement.children
    domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(domElement)
+    container.appendChild(jkhgjlhk)
 }
 
 const reactElement = {
