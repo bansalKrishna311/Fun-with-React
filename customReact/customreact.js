993f39e7f851add38e5f66c86f60c1efb47145b0
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-27 23:42:20 +0530

    chatgpt

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,19 +1,46 @@
-function customRender(reactElement, container) {
-    const domElement = document.createElement(reactElement.type)
-   domElement.innerHTML = reactElement.children
-   domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(domElement)
-}
+// function customRender(reactElement, container) {
+//     const domElement = document.createElement(reactElement.type)
+//    domElement.innerHTML = reactElement.children
+//    domElement.setAttribute('href', reactElement.props.href)
+//     domElement.setAttribute('target', reactElement.props.target)
+//     container.appendChild(domElement)
+// }
 
-const reactElement = {
-    type : 'a',
-    props : {
-        href: 'https://www.google.com/',
-        target: '_blank',
-    },
-    children : 'Click Here'
-}
+// const reactElement = {
+//     type : 'a',
+//     props : {
+//         href: 'https://www.google.com/',
+//         target: '_blank',
+//     },
+//     children : 'Click Here'
+// }
 
-const mainContainer = document.querySelector('#root')
+// const mainContainer = document.querySelector('#root')
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to render the custom React element
+    function customRender(reactElement, container) {
+        const domElement = document.createElement(reactElement.type);
+        domElement.innerHTML = reactElement.children;
+        domElement.setAttribute('href', reactElement.props.href);
+        domElement.setAttribute('target', reactElement.props.target);
+        container.appendChild(domElement);
+    }
+
+    // Define the React element
+    const reactElement = {
+        type: 'a',
+        props: {
+            href: 'https://www.google.com/',
+            target: '_blank',
+        },
+        children: 'Click Here'
+    };
+
+    // Select the container
+    const mainContainer = document.querySelector('#root');
+
+    // Render the React element
+    customRender(reactElement, mainContainer);
+});
