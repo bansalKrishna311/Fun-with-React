bde0bcc3f6cb53b0a709931a9d6493b47e2a95a9
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-27 23:44:24 +0530

    koi bkl hi hoga '

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -4,7 +4,7 @@ function customRender(reactElement, container) {
    domElement.innerHTML = reactElement.children
    domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(domElement)
+
 }
 
 // const reactElement = {
