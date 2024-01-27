a8340866dd323bcb922c3ff9966f339c309b082d
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-27 23:39:55 +0530

    koshish jaaari hei par lund kuchh ni hora

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -3,7 +3,7 @@ function customRender(reactElement, container) {
    domElement.innerHTML = reactElement.children
    domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(jkhgjlhk)
+    container.appendChild(domElement)
 }
 
 const reactElement = {
