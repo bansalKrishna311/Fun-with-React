0b17f1b5a65c818a4d2d01265aca244098ae41ad
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-28 10:15:17 +0530

    props

--- 03_TailwindProps/src/App.jsx
+++ 03_TailwindProps/src/App.jsx
@@ -3,15 +3,20 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/Card";
-function App() {
+function App(props) {
+  console.log(props.userName)
   const [count, setCount] = useState(0);
+    let myObj = {
+      userName :'krishna',
+      age:21
+    }
 
   return (
     <>
       <h1 className="bg-green-400 text-blue-950 px-10 rounded-xl">hello</h1>
 
-      <Card />
-      <Card />
+      <Card channel = "chaiaurcode" myArr = {[1, 2, 3]} />
+      <Card channel = "chaiaurcode" obj = {myObj}  />
       <Card />
 
     </>
