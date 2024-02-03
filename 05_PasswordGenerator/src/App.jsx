fd6092a4db2ff4c203583bc92042d8c68520755b
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-02-03 23:56:06 +0530

    css ki bakchodi

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -28,7 +28,16 @@ function App() {
 
   return (
     <>
-<div className="w-full max-w-md"></div>
+<div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500"> 
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input type="text"
+      value={password} 
+      className='outline-none w-full py-1 px-3 '
+      placeholder = 'Password'
+      readonly
+      />
+    </div>
+</div>
     </>
   )
 }
