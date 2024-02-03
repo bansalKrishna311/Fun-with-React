83a018b93ca1d82f780bf3caeb46e67fb4c38158
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-02-04 00:06:53 +0530

    passgenerator  htm,l

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [length, setlength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [characterAllowed, setCharacterAllowed] = useState(false)
-  const [password, setPassword] = useState(false)
+  const [password, setPassword] = useState("")
  const passwordGenerator = useCallback(()=> {
   let pass = ""
   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -29,13 +29,27 @@ function App() {
   return (
     <>
 <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500"> 
+<h1 className='text-white text-center py-5'>Password Generator</h1>
+
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
       <input type="text"
       value={password} 
-      className='outline-none w-full py-1 px-3 '
-      placeholder = 'Password'
-      readonly
+      className="outline-none w-full py-1 px-3"
+      placeholder = "Password"
+      readOnly
+
       />
+      <button className="outline-none bg-blue-700 text-white px-3 py-05 shrink=0">Copy</button>
+    </div>
+    <div className="flex text-sm gap-x-2">
+      <div className="flex items-center gap-x-1">
+        <input type="range"
+        min={6}
+        max={10}
+        value={length}
+        />
+
+      </div>
     </div>
 </div>
     </>
