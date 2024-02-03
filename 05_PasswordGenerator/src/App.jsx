c90d552954e4a267f1ed22ace9dad3d6a672b690
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-02-04 00:10:39 +0530

    left at 30:00 would suggest to make the password generator from scratch

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -47,7 +47,10 @@ function App() {
         min={6}
         max={10}
         value={length}
+        className='cursor-pointer'
+        onChange={(e) => {setlength(e.target.value)}}
         />
+        <label>Length:{length}</label>
 
       </div>
     </div>
