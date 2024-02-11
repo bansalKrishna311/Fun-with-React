bc69a6be0b758dfd4c0cffc402dfb8ee76ad23ca
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-02-11 23:33:31 +0530

    pata nhi still

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -22,10 +22,7 @@ function App() {
     setPassword(pass)
 
   }, [length, numberAllowed, characterAllowed, setPassword])
-
-
-
-passwordGenerator()
+      passwordGenerator()
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500">
