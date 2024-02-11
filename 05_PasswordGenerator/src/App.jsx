57be276fa847c5a39aad557c3c56bef777419274
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-02-11 23:17:34 +0530

    refactor

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -8,53 +8,53 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [characterAllowed, setCharacterAllowed] = useState(false)
   const [password, setPassword] = useState("")
- const passwordGenerator = useCallback(()=> {
-  let pass = ""
-  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-  if(numberAllowed) str +="0123456789"
-  if(characterAllowed) str+="!@#$%^&*()_+-={}[]:';<>?,./~`"
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (characterAllowed) str += "!@#$%^&*()_+-={}[]:';<>?,./~`"
 
-    for(let i = 1;  i<=Array,length; i++){
-      let char = Math.floor(Math.random()*str.length + 1)
+    for (let i = 1; i <= Array, length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
       pass = str.charAt(char)
 
     }
     setPassword(pass)
 
- }, [length, numberAllowed, characterAllowed, setPassword])
-  
+  }, [length, numberAllowed, characterAllowed, setPassword])
 
-  
+
+
 
   return (
     <>
-<div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500"> 
-<h1 className='text-white text-center py-5'>Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500">
+        <h1 className='text-white text-center py-5'>Password Generator</h1>
 
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
-      <input type="text"
-      value={password} 
-      className="outline-none w-full py-1 px-3"
-      placeholder = "Password"
-      readOnly
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
 
-      />
-      <button className="outline-none bg-blue-700 text-white px-3 py-05 shrink=0">Copy</button>
-    </div>
-    <div className="flex text-sm gap-x-2">
-      <div className="flex items-center gap-x-1">
-        <input type="range"
-        min={6}
-        max={10}
-        value={length}
-        className='cursor-pointer'
-        onChange={(e) => {setlength(e.target.value)}}
-        />
-        <label>Length:{length}</label>
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-05 shrink=0">Copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range"
+              min={6}
+              max={10}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setlength(e.target.value) }}
+            />
+            <label>Length:{length}</label>
 
+          </div>
+        </div>
       </div>
-    </div>
-</div>
     </>
   )
 }
