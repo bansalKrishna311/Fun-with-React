7e05ab8fcf7a5ac518e8c1ddafd94bcfe73cf9f8
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-05 12:34:30 +0530

    Commented out useState hook for count variable

--- 03_TailwindProps/src/App.jsx
+++ 03_TailwindProps/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import Card from "./components/Card";
 function App(props) {
   console.log(props.userName)
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
     let myObj = {
       userName :'krishna',
       age:21
