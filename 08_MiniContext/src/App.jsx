ede1caf469cf63270a87cbcb1131f6adebc60ea2
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-06 15:30:14 +0530

    basic work is done

--- 08_MiniContext/src/App.jsx
+++ 08_MiniContext/src/App.jsx
@@ -2,10 +2,12 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   return (
    <>
+   <UserContextProvider></UserContextProvider>
    <h1>Lorem ipsum dolor sit amet.</h1>
    </>
   )
