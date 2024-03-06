93697ee3cf26479672c4969113e80e9509618ae2
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-06 12:14:29 +0530

    changing and using another way of routing

--- 07_React_Router_Dom/src/main.jsx
+++ 07_React_Router_Dom/src/main.jsx
@@ -2,36 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/home/home.jsx'
 import About from './components/about/about.jsx'
 import Contact from './components/contact/Contact.jsx'
 
-const router = createBrowserRouter([
-  {
-    path :'/',
-    element : <Layout/>,
-    children:[
-      {
-        path:"",
-        element:<Home/>
-      },
-      {
-        path:"About",
-        element:<About/>
-      },
-      {
-        path:"Contact",
-        element:<Contact />
-      },
-    ]
+// const router = createBrowserRouter([
+//   {
+//     path :'/',
+//     element : <Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"About",
+//         element:<About/>
+//       },
+//       {
+//         path:"Contact",
+//         element:<Contact />
+//       },
+//     ]
 
-  }
+//   }
+// ])
+
+
+const router = createBrowserRouter([
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='About' element={<About/>}/>
+    <Route path='Contact' element={<Contact/>}/>
+    </Route>
+
+  )
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
