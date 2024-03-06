ecd80262dcab32ff63ac289b76a0eccd78625fa9
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-06 12:40:14 +0530

    github page is done

--- 07_React_Router_Dom/src/components/Github/Github.jsx
+++ 07_React_Router_Dom/src/components/Github/Github.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 function Github() {
+    const[data,setData]  = useState([])
+    useEffect(() => {
+      fetch('https://api.github.com/users/bansalkrishna311')
+      .then(Response => Response.json())
+      .then(data =>{
+        console.log(data);
+        setData(data)
+      })
+
+    }, [])
+
+
+    
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers:</div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data.followers}
+    <img  src={data.avatar_url} alt="git Picture" width={300} /></div>
   )
 }
 
