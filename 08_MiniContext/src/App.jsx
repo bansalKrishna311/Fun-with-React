760201cd8bdb3af978fae8ce3522a4568e218dfc
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-06 15:37:29 +0530

    pata ni kya chasl rah hei error aare hei

--- 08_MiniContext/src/App.jsx
+++ 08_MiniContext/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
   return (
    <>
    <UserContextProvider></UserContextProvider>
    <h1>Lorem ipsum dolor sit amet.</h1>
+   <Login />
+   <Profile />
    </>
   )
 }
