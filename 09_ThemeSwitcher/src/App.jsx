9503d377a31b04c8c99b881997edb550aed0b993
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-07 12:54:16 +0530

    Add ThemeBtn and Card components

--- 09_ThemeSwitcher/src/App.jsx
+++ 09_ThemeSwitcher/src/App.jsx
@@ -2,6 +2,8 @@
 import { useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
   constp[themeMode, setThemeMode] = useState("Light")
@@ -25,12 +27,12 @@ function App() {
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                      {/* ThemeBtn */}
+                     <ThemeBtn />
                         
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                        {/* Card */}
+                    <Card />
                     </div>
                 </div>
             </div>
