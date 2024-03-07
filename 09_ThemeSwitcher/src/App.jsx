67dfe4f7ec4118aacb70b1fd6677ce1974e5613e
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-07 12:50:17 +0530

    basic functionality has been made

--- 09_ThemeSwitcher/src/App.jsx
+++ 09_ThemeSwitcher/src/App.jsx
@@ -1,9 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
 
 function App() {
+  constp[themeMode, setThemeMode] = useState("Light")
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  // actualchange in  theme 
+  useEffect(() => {
+  document.querySelector('html').classList.remove("light","dark")
+  document.querySelector('html').classList.add(themeMode)
 
+  }, [themeMode])
+   
 
   return (
    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
