79dbe08a2c465bde062049643f9c13257ebf43b6
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-18 12:15:41 +0530

    dpnee

--- 10_ToDoContextLocal/src/App.jsx
+++ 10_ToDoContextLocal/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts/TodoContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodo] = useState([])
+  const addTodo = (todo)=>{
+    setTodo((prev) =>[{id:Date.now(), ...todo}, ...prev])
+
+  }
+  const updateTodo = (id,todo)=>{
+    setTodo((prev)=> prev.map((prevTodo) =>(prevTodo.id === id ? todo: prevTodo)))
+  }
 
   return (
-    <>
+    <TodoProvider value={{todos, addTodo,updateTodo,deleteTodo,toggleComplete} }>
 <div className="bg-[#172842] min-h-screen py-8">
   <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-5 text-white">
     <h1 className="text-2xl font-bold text-center mb-8 mt-2"> Manange Your Todos</h1>
@@ -21,7 +29,7 @@ function App() {
 
   </div>
 </div>
-    </>
+    </TodoProvider>
   )
 }
 
