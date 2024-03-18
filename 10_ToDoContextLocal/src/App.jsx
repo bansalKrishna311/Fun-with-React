5747b092ec00963044bd16038aadef7951056cba
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-18 16:03:42 +0530

    Update Card image and add TodoProvider component

--- 10_ToDoContextLocal/src/App.jsx
+++ 10_ToDoContextLocal/src/App.jsx
@@ -11,17 +11,33 @@ function App() {
 
   }
   const updateTodo = (id,todo)=>{
-    setTodo((prev)=> prev.map((prevTodo) =>(prevTodo.id === id ? todo: prevTodo)))
+    setTodo((prev)=> prev.map((prevTodo) =>(prevTodo.id === id ? todo: prevTodo))) 
+   
+     
+   
   }
-
+  const deleteTodo = (id) => {
+    setTodo((prev) => prev.filter((todos) => todos.id !== id))
+  }
+  const toggleComplete = (id) => {
+    setTodo((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)) 
+  }
+ 
   return (
-    <>
-   <h1 className="text-3xl font-bold underline">
-    Hello World!!
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+   <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
 
-   </h1>
-
-    </>
+    </TodoProvider>
   )
 }
 
