1ebaf070fa8c606b964e68eb98279941137344d6
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-21 09:37:28 +0530

    Fix form submission and update todo value

--- 10_ToDoContextLocal/src/Components/TodoForm.jsx
+++ 10_ToDoContextLocal/src/Components/TodoForm.jsx
@@ -9,14 +9,18 @@ function TodoForm() {
         e.preventDefault()
         if(!todo) return
         addTodo({ todo, completed:false})
+        setTodo("")
+
     }
 
     return (
-        <form  className="flex">
+        <form  className="flex"  onSubmit={add}>
             <input
+               onChange={(e) => setTodo(e.target.value)}
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={todo}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
