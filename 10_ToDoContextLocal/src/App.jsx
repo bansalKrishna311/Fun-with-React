6b8099028a1643aa5bceb064014b0bd945e00e4a
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-21 09:56:57 +0530

    Fix bug in login functionality

--- 10_ToDoContextLocal/src/App.jsx
+++ 10_ToDoContextLocal/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './contexts/TodoContext'
+import TodoForm from './Components/TodoForm'
 
 function App() {
   const [todos, setTodo] = useState([])
@@ -45,10 +46,14 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id}> 
+                        
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
