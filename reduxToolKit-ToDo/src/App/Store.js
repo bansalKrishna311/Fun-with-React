be759f2379fdcd56d99d2e137e8d40a9b8ec8fd8
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-03-24 11:31:35 +0530

    Update dependencies in package.json

---
+++ reduxToolKit-ToDo/src/App/Store.js
@@ -0,0 +1,17 @@
+import{ConfigureStore} from '@reduxjs/toolkit';
+
+export const store = ConfigureStore({})
+
+
+
+
+
+
+
+
+
+
+
+
+// step 1 import configure store from redux toolkit
+// step 2 export configure store 
